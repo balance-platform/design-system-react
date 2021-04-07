6e2b2b414a393c4b1f9e70b604541881bbf54cfb
Mikhail Lipilin <mikhail.lipilin@balance-pl.ru>
2021-04-07 09:55:41 +0300

    remove unnecessary boilerplate app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode>Demo Page</React.StrictMode>,
   document.getElementById('root')
 )
 
